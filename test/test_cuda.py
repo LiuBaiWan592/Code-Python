0f2021795bee7f260102725df421dc7ac319e835
LiuBaiWan-LEGION <liubaiwan592@outlook.com>
2025-04-02 23:11:25 +0800

    Update file

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -4,3 +4,6 @@ if torch.cuda.is_available():
     print("CUDA is available")
 else:
     print("CUDA is not available")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
